099edfa930a8609008d61adacf54931076f1b7c3
Gaya Aviv <gayaav@post.bgu.ac.il>
2024-12-21 18:57:42 +0200

    .

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -2,6 +2,7 @@ package bgu.spl.mics;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * The {@link MessageBusImpl} class is the implementation of the MessageBus interface.
@@ -12,10 +13,12 @@ public class MessageBusImpl implements MessageBus {
 
 
 	private static MessageBusImpl instance = null; // field for singleton
+
 	private final Map<MicroService, Queue<Message>> microServices;
-	private final Map<Class<? extends Broadcast>, List<MicroService>> broadcasts;
+	private final Map<Class<? extends Broadcast>, List<MicroService>> broadcasts; //לבדוק אם צריך שגם הרשימה הפנימית תיהיה אטומית
 	private final Map<Class<? extends Event<?>>, List<MicroService>> events;
-	//another fields
+
+	private final Map<Event<?>, Future<?>> eventFuture = new ConcurrentHashMap<>();
 
 	private MessageBusImpl() {
 		microServices = new ConcurrentHashMap<>();
@@ -29,6 +32,7 @@ public class MessageBusImpl implements MessageBus {
 		}
 		return instance;
 	}
+
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
 		if(events.containsKey(type)){
@@ -55,22 +59,41 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
-		// TODO Auto-generated method stub
+		synchronized (eventFuture) {
+			Future<T> future = (Future<T>) eventFuture.get(e);
+		if (future != null) {
+			future.resolve(result);
+			eventFuture.remove(e);
+		}
+			}
 
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		List<MicroService> microServiceList =broadcasts.get(b);
-		for(MicroService m : microServiceList){
-			addMsg(m,b);
+		List<MicroService> microServiceList = broadcasts.get(b);
+		synchronized (microServiceList) {
+			for (MicroService m : microServiceList) {
+				addMsg(m, b);
+			}
 		}
 	}
 
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// TODO Auto-generated method stub
-		return null;
+		List<MicroService> microServiceList = events.get(e.getClass());
+		synchronized (microServiceList) {
+			if (microServiceList.isEmpty()) { //if nobody subscribe this event
+				return null;
+			}
+
+			//TODO: לממש את הרובין פשיין כדי לבחור למי מהמיקרוסרוויסים להביא אתת הevent
+
+			Future<T> future = new Future<>(); //Create future for the event
+			eventFuture.put(e, future);
+
+			return future;
+		}
 	}
 
 	@Override
@@ -82,18 +105,39 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void unregister(MicroService m) {
-		// TODO Auto-generated method stub
 
+		microServices.remove(m); //Remove from MicroServices Map
+
+		for(List<MicroService> msList : broadcasts.values()){ //Remove from Broadcasts Map, if in
+			synchronized (msList){
+				msList.remove(m);
+			}
+		}
+
+		for(List<MicroService> msList : events.values()){ //Remove from Events Map, if in
+			synchronized (msList){
+				msList.remove(m);
+			}
+		}
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			while (microServices.get(m).isEmpty()) {
+				wait();
+			}
+		}
+		catch (InterruptedException e){
+			Thread.currentThread().interrupt();
+		}
+
+		return microServices.get(m).remove();
 	}
 
 	private void addMsg(MicroService m, Message msg) {
 		microServices.get(m).add(msg);
+		notifyAll();
 	}
 	
 
