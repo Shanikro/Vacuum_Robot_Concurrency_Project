c489405ddc15199997b0bb81eda261cfdff40db1
shani <shanikro@post.bgu.ac.il>
2024-12-24 20:53:58 +0200

    /

--- src/main/java/bgu/spl/mics/application/services/CameraService.java
+++ src/main/java/bgu/spl/mics/application/services/CameraService.java
@@ -3,6 +3,7 @@ package bgu.spl.mics.application.services;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.DetectObjectsEvent;
+import bgu.spl.mics.application.messages.TerminatedBroadcast;
 import bgu.spl.mics.application.messages.TickBroadcast;
 import bgu.spl.mics.application.objects.Camera;
 import bgu.spl.mics.application.objects.StampedDetectedObjects;
@@ -42,33 +43,23 @@ public class CameraService extends MicroService {
     @Override
     protected void initialize() {
 
-        // Handle TickBroadcast
+        //Subscribe TickBroadcast
         subscribeBroadcast(TickBroadcast.class, tick -> {
             currentTick = tick.getTime();
-
             int detectedTime = currentTick - camera.getFrequency();
+
             if (detectedTime>0) {
                 StampedDetectedObjects detectedObjects = camera.detectObjects(detectedTime);
 
-                // Check if there is objects and the camera is on
+                //Check if there is objects and the camera is on
                 if (detectedObjects != null && camera.isUp()) {
                     // Send event with detected objects
-                    Future<Boolean> futureObject = (Future<Boolean>) sendEvent(new DetectObjectsEvent(getName(),detectedTime, detectedObjects.getDetectedObjects()));
-
-                    if(futureObject.get(tick.getDuration()-tick.getTime(), TimeUnit.MILLISECONDS)) {
-                        StatisticalFolder.updateDetectedObjects(camera.getDetectedObjectsCount());
-                    }
-                }
-
-                // Handle errors
-                if (!camera.isUp()) {
-                    sendBroadcast(new TerminatedBroadcast());
-                    terminate();
+                    sendEvent(new DetectObjectsEvent(getName(),detectedTime, detectedObjects.getDetectedObjects()));
                 }
             }
         });
 
-        // Handle TerminatedBroadcast
+        //Subscribe Terminated Broadcast
         subscribeBroadcast(TerminatedBroadcast.class, t -> terminate());
     }
 
